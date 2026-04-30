1b9fd53bd6b19ee7c1490e925bf05a90b267b069
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-04-29 22:35:31 -0300

    adiciona cabecalho

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
  
 int main(){
+    printf("=======================\n");
+    printf("\t\tNOMES\n");
+    printf("=======================\n\n");
+
     printf("---------Ligia---------");
     printf("Kauanne");
     return 0;
